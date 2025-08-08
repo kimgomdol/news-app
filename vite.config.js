c56584f1a53265ba577804f6940d334db6de556b
kimgomdol <pandadadac@gmail.com>
2025-08-08 23:49:55 +0900

    Fix CSS path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/news-app/',
+  base: '/news-app/', // GitHub Pages 경로
   build: {
-    outDir: 'docs'
+    outDir: 'docs',   // Pages에서 docs 폴더 사용
+    emptyOutDir: true // 빌드 시 기존 파일 삭제
   }
 });
