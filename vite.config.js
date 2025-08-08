9b85086b5846da92498427987b4de23cde0006e1
kimgomdol <pandadadac@gmail.com>
2025-08-08 23:36:18 +0900

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// GitHub Pages에 배포 시, base를 저장소 이름으로 설정
 export default defineConfig({
-  base: '/news-app/', // ✅ GitHub Pages 저장소명
   plugins: [react()],
-})
+  base: '/news-app/', // GitHub Pages 경로
+  build: {
+    outDir: 'docs' // Pages에서 docs 폴더를 빌드 출력
+  }
+});
