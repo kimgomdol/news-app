0b1653554bc71971882fdea8999d98fe6ca10773
kimgomdol <pandadadac@gmail.com>
2025-08-08 23:46:39 +0900

    Rebuild with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/news-app/', // GitHub Pages 서브 디렉토리 경로
+  base: '/news-app/',
   build: {
     outDir: 'docs'
   }
-})
+});
