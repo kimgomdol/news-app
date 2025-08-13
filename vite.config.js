0ca6f65969b58c9cf2ec43f664953bff41989aa3
Kimgomdol <pandadadac@gmail.com>
2025-08-14 00:37:54 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/news-app/' // ⭐ GitHub 저장소 이름
-})
+  base: '/news-app/' // GitHub repo name
+});
